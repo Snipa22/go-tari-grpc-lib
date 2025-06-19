e2e553802eccb344c4db01a06ddb3d938105e140
Alexander Blair <git@jagtech.io>
2025-06-18 20:39:04 -0700

    feat(cmd): add walletSweeper
    Add a sweep command that uses GRPC, non-ideal, but good for wallet
     management

---
+++ cmd/walletSweeper/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Snipa22/go-tari-grpc-lib/v2/tari_generated"
+	"github.com/Snipa22/go-tari-grpc-lib/v2/walletGRPC"
+	"log"
+)
+
+func main() {
+	walletGRPCAddressPtr := flag.String("wallet-grpc-address", "127.0.0.1:18143", "Tari wallet GRPC address")
+	destAddressPtr := flag.String("dest-address", "", "Destination address for sweeping to")
+	flag.Parse()
+	walletGRPC.InitWalletGRPC(*walletGRPCAddressPtr)
+	balances, err := walletGRPC.GetBalances()
+	if err != nil {
+		log.Fatal(err)
+	}
+	remainder := balances.AvailableBalance % 100000
+	toSend := balances.AvailableBalance - remainder
+	fmt.Printf("Sending %v uT to %v\n", toSend, *destAddressPtr)
+	resp, err := walletGRPC.SendTransactions([]*tari_generated.PaymentRecipient{
+		{
+			Address:     *destAddressPtr,
+			Amount:      toSend,
+			FeePerGram:  5,
+			PaymentType: 2,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Transactions sent successfully: %v, error if exists: %v\n", resp.Results[0].IsSuccess, resp.Results[0].FailureMessage)
+}
