00f4b299f1eebb6c00a628a3bf9959dafe1e0811
Alexander Blair <git@jagtech.io>
2025-06-18 20:42:38 -0700

    feat(walletSweeper): safety checks
    Add a low balance check
    Add a check for address being set

--- cmd/walletSweeper/main.go
+++ cmd/walletSweeper/main.go
@@ -12,6 +12,9 @@ func main() {
 	walletGRPCAddressPtr := flag.String("wallet-grpc-address", "127.0.0.1:18143", "Tari wallet GRPC address")
 	destAddressPtr := flag.String("dest-address", "", "Destination address for sweeping to")
 	flag.Parse()
+	if *destAddressPtr == "" {
+		log.Fatal("Destination address is required")
+	}
 	walletGRPC.InitWalletGRPC(*walletGRPCAddressPtr)
 	balances, err := walletGRPC.GetBalances()
 	if err != nil {
@@ -19,6 +22,10 @@ func main() {
 	}
 	remainder := balances.AvailableBalance % 100000
 	toSend := balances.AvailableBalance - remainder
+	if toSend <= 1000000 {
+		fmt.Println("Balance to send is less than 1 XTM, not sweeping.")
+		return
+	}
 	fmt.Printf("Sending %v uT to %v\n", toSend, *destAddressPtr)
 	resp, err := walletGRPC.SendTransactions([]*tari_generated.PaymentRecipient{
 		{
